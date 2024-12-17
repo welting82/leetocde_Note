716de39ae832775e0ac245732f2041491c4034df
WeiTingChou <WeiTingChou@fih-foxconn.com>
2024-12-17 09:35:43 +0800

    12/16

---
+++ src/2593.find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=2593 lang=cpp
+ *
+ * [2593] Find Score of an Array After Marking All Elements
+ */
+
+// @lc code=start
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        long long res = 0;
+        int n = nums.size(), cnt = 0;
+
+        return res;
+    }
+};
+// @lc code=end
+/*
+[5,1,1,7,2,4]\n
+*/
